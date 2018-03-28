60f5fc9f40ced222fdb9f5a5ba3c4130828b1df0
Manuel Jung <manuel.jung@hs.uni-hamburg.de>
2018-03-28 13:31:09 +0200

    Remove the call to __init__()

--- coroutine.py
+++ coroutine.py
@@ -10,10 +10,8 @@ except ImportError:
 
 
 def coroutine(func):
-    #@functools.wraps(func)
     class wrapper(IterableUserDict):
         def __init__(self, *args, **kwargs):
-            super(IterableUserDict).__init__()
             self.target = None
             self.func = func
             self.data = dict()
